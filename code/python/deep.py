3fc614a899ead8a347268c4e9dd6dab7ea7b528b
tothantal <43417284+tothantal@users.noreply.github.com>
2022-11-08 15:12:04 +0100

    Update deep.py

--- code/python/deep.py
+++ code/python/deep.py
@@ -19,9 +19,7 @@ ds, info = tfds.load('nyu_depth_v2',
                      with_info = True,
                      shuffle_files = True)
 
-ds_test = ds[0]
 ds_train = ds[1]
-ds_train_batch = ds_train.batch(32)
 
 # Extract numpy.array X & y from tf.data.Dataset
 X_numpy = np.asarray(list(map(lambda x: x[0], tfds.as_numpy(ds_train))))
@@ -29,14 +27,14 @@ y_numpy = np.asarray(list(map(lambda x: x[1], tfds.as_numpy(ds_train))))
 X_train, X_test, y_train, y_test = train_test_split(X_numpy, y_numpy, 
                                                     test_size=0.2, 
                                                     random_state=42)
-y_train = to_categorical(y_train, 30)
-y_test = to_categorical(y_test, 30)
+y_train = to_categorical(y_train, 20)
+y_test = to_categorical(y_test, 20)
 
 # Creating Model
 model = tf.keras.Sequential([
-    tf.keras.layers.Dense(100, activation = tf.nn.relu, input_shape = (480, 640, 3)),    
-    tf.keras.layers.Dense(100, activation = tf.nn.relu),
-    tf.keras.layers.Dense(30, activation = 'softmax')
+    tf.keras.layers.Dense(3, activation = tf.nn.relu, input_shape = (480, 640, 3)),    
+    tf.keras.layers.Dense(10, activation = tf.nn.relu),
+    tf.keras.layers.Dense(20, activation = 'softmax')
 ])
 
 # F1
