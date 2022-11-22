bbdb1a1fdc6b6a20e2c38ce05623891dc0c15f17
tothantal <43417284+tothantal@users.noreply.github.com>
2022-11-22 13:38:39 +0100

    Create metrics.py

---
+++ code/python/deep/metrics.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 18 15:38:43 2022
+
+@author: Tóth Antal
+"""
+
+import keras.backend as K
+
+def f1(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    recall = true_positives / (possible_positives + K.epsilon())
+    f1_val = 2 * (precision * recall) / (precision+recall + K.epsilon())
+    return f1_val
